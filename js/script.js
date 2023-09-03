9f10bea3802a6449c73ef3ed77c445ccc42704f6
lunajose0 <josejavier.farias@gmail.com>
2023-09-03 20:28:05 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// En este archivo no utilizamos el evento "DOMContentLoaded", ya que se colocó el atributo "defer" en la importación del script,
+// que nos soluciona el problema de los elementos no cargados del DOM. Más info => https://www.w3schools.com/tags/att_script_defer.asp
+
 const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mostrar
 
 const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
@@ -11,15 +14,14 @@ function showData(dataArray) {
   // El for itera sobre los elementos del array
   for (const item of dataArray) {
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
-    const html = `<p> ${item.name} ${item.lastname} </p>`;
-
-    // Se agrega el párrafo al innerHTML del contenedor
-    container.innerHTML += html;
+    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
 
+// Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
 // Fetch al archivo con los datos
-fetch(DATA_URL)
+fetch("json/data.json")
   // Cuando la solicitud se completa
   .then(response => response.json())
   // Cuando la respuesta es válida
